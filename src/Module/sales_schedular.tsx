06c9bf2245abb08af6cfe035fa7bbdbc0252dcfb
Vipul Nagarkar <vipulnagarkar102@email.com>
2026-03-28 14:34:36 +0530

    schedular page started

--- src/Module/sales_schedular.tsx
+++ src/Module/sales_schedular.tsx
@@ -5,12 +5,12 @@ import SalesScheduler from '../components/sales_dashboard/SalesScheduler';
 
 const SalesSchedularPage: React.FC = () => {
   return (
-    <div className="flex h-screen bg-white overflow-hidden">
+    <div className="flex  bg-white ">
       <Sidebar />
-      <div className="flex-1 flex flex-col min-w-0 bg-white h-screen overflow-hidden">
+      <div className="flex-1 flex flex-col min-w-0 bg-white ">
         <Topbar />
         <div className="h-10 shrink-0" /> {/* Fixed Topbar Spacer */}
-        <main className="flex-1 ml-16 relative overflow-hidden">
+        <main className="flex-1 ml-16 relative ">
           <SalesScheduler />
         </main>
       </div>
